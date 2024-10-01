2b4eb7aa9cc73d799a68fa15d8833d7bf4b4c6e0
Ignacio Carazo <ignaciocarazo@Ignacios-MacBook-Pro-2.local>
2024-10-01 15:51:14 -0600

    add for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/docs-soa/',  // Update this line to match your GitHub repository name
   plugins: [react()],
-})
+});
