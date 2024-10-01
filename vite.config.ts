afa09c37659fc54a47c94e95a64aef378528c74a
Ignacio Carazo <ignaciocarazo@Ignacios-MacBook-Pro-2.local>
2024-10-01 15:53:36 -0600

    remove dist from ignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/docs-soa/',  // Update this line to match your GitHub repository name
+  base: '/docs-soa',  // Update this line to match your GitHub repository name
   plugins: [react()],
 });
